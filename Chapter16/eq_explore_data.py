44aa69aacb63ba5b2419232d5eebba18d261332d
wiktorm3n <46652899+wiktorm3n@users.noreply.github.com>
2021-07-25 16:46:41 +0200

    Add files via upload

--- Chapter16/eq_explore_data.py
+++ Chapter16/eq_explore_data.py
@@ -1,11 +1,37 @@
 import json
+from plotly import offline
+import plotly.graph_objects as plt
 
 '''Explore the structure of the data'''
 filename ='data/eq_data_1_day_m1.json'
 with open(filename) as f:
-    all_data = json.load(f)
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
 
 readable_file = 'data/readable_eq_data.json'
 with open(readable_file,'w') as f:
     json.dump(all_eq_data,f,indent=4)
+
+mags,lons,lats = [],[],[]
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+
+print(mags[:10])
+print(lons[:5])
+print(lats[:5])
+
+'''Map the earthquakes'''
+data = [plt.Scattergeo(lon=lons, lat=lats)]
+my_layout = plt.Layout(title='Global Earthquakes')
+
+
+fig = {'data':data,'layout':my_layout}
+offline.plot(fig,filename='global_earthquakes.html')
+
     
