0eb61abdadf6a4413d78131f4a698733689f897d
wiktorm3n <46652899+wiktorm3n@users.noreply.github.com>
2021-07-26 14:07:51 +0200

    Try It 16_7

---
+++ Chapter16/TryIt16_7.py
@@ -0,0 +1,43 @@
+import json
+from plotly import offline
+from plotly import colors
+import plotly.graph_objects as plt
+
+'''Explore the structure of the data'''
+filename = 'data/eq_data_30_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+readable_file = 'data/readable_eq_data.json'
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4)
+
+mags, lons, lats, hover_texts = [], [], [], []
+for eq_dict in all_eq_dicts:
+    title = eq_dict['properties']['title']
+    mags.append(eq_dict['properties']['mag'])
+    lons.append(eq_dict['geometry']['coordinates'][0])
+    lats.append(eq_dict['geometry']['coordinates'][1])
+    hover_texts.append(eq_dict['properties']['title'])
+
+'''Map the earthquakes'''
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'}
+    },
+}]
+title_my_layout = all_eq_data['metadata']['title']
+my_layout = plt.Layout(title= title_my_layout)
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
