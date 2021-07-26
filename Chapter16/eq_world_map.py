33e7c86ec2ff38e8acb35ac18ad84d3c00563c4b
wiktorm3n <46652899+wiktorm3n@users.noreply.github.com>
2021-07-26 13:42:14 +0200

    Before TryIt 16.6

--- Chapter16/eq_world_map.py
+++ Chapter16/eq_world_map.py
@@ -4,42 +4,45 @@ from plotly import colors
 import plotly.graph_objects as plt
 
 '''Explore the structure of the data'''
-filename ='data/eq_data_30_day_m1.json'
+filename = 'data/eq_data_30_day_m1.json'
 with open(filename) as f:
     all_eq_data = json.load(f)
 
 all_eq_dicts = all_eq_data['features']
 
 readable_file = 'data/readable_eq_data.json'
-with open(readable_file,'w') as f:
-    json.dump(all_eq_data,f,indent=4)
+with open(readable_file, 'w') as f:
+    json.dump(all_eq_data, f, indent=4)
 
-mags,lons,lats = [],[],[]
+mags, lons, lats, hover_texts = [], [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
     lon = eq_dict['geometry']['coordinates'][0]
     lat = eq_dict['geometry']['coordinates'][1]
+    title = eq_dict['properties']['title']
     mags.append(mag)
     lons.append(lon)
     lats.append(lat)
+    hover_texts.append(title)
 
 '''Map the earthquakes'''
 data = [{
     'type': 'scattergeo',
     'lon': lons,
     'lat': lats,
-    'marker':{
-        'size' : [5*mag for mag in mags],
-        'color' : mags,
-        'colorscale' : 'Viridis',
+    'text' : hover_texts,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Viridis',
         'reversescale': True,
-        'colorbar' : {'title' : 'Magnitude'}
+        'colorbar': {'title': 'Magnitude'}
     },
 }]
 my_layout = plt.Layout(title='Global Earthquakes')
 
-fig = {'data':data,'layout':my_layout}
-offline.plot(fig,filename='global_earthquakes.html')
-    
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='global_earthquakes.html')
+
 for key in colors.PLOTLY_SCALES.keys():
     print(key)
